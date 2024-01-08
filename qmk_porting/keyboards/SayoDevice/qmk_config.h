b3feead78fcf7112bf4d4abd2d2052958b1a903e
Matt <matt@deity.co.nz>
2024-01-08 21:45:07 +1300

    aever

--- qmk_porting/keyboards/SayoDevice/qmk_config.h
+++ qmk_porting/keyboards/SayoDevice/qmk_config.h
@@ -33,7 +33,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
     }
 #define MATRIX_COL_PINS                                                                                \
     {                                                                                                  \
-         B15 \
+         B14 \
     }
 //#define DYNAMIC_KEYMAP_LAYER_COUNT 10
 

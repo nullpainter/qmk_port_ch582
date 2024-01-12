116c7eb96a86c4e8d996e2cbc9283233021e531c
Matt Painter <matt@deity.co.nz>
2024-01-12 18:17:35 +1300

    Update qmk_config.h

--- qmk_porting/keyboards/SayoDevice/qmk_config.h
+++ qmk_porting/keyboards/SayoDevice/qmk_config.h
@@ -33,7 +33,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
     }
 #define MATRIX_COL_PINS                                                                                \
     {                                                                                                  \
-         B1 \
+         B16 \
     }
 //#define DYNAMIC_KEYMAP_LAYER_COUNT 10
 

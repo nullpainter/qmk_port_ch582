7395b50d9a1965739ce8e94bf074e6eb71035402
Matt Painter <matt@deity.co.nz>
2024-01-12 19:27:25 +1300

    Update keymap.c

--- qmk_porting/keyboards/SayoDevice/keymaps/default/keymap.c
+++ qmk_porting/keyboards/SayoDevice/keymaps/default/keymap.c
@@ -46,13 +46,13 @@ const uint32_t PROGMEM unicode_map[] = {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT_all(/* 0: qwerty */
-                     KC_1, KC2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_A, KC_B, KC_C),
+                     KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_A, KC_B, KC_C),
     [1] = LAYOUT_all(/* 1: fn */
-                        KC_1, KC2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_A, KC_B, KC_C)),
+                        KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_A, KC_B, KC_C)),
   [2] = LAYOUT_all(/* 2: fn */
-                         KC_1, KC2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_A, KC_B, KC_C)),
+                         KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_A, KC_B, KC_C)),
   [3] = LAYOUT_all(/* 3: fn */
-                       KC_1, KC2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_A, KC_B, KC_C)),
+                       KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_A, KC_B, KC_C)),
  
  
  
